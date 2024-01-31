ca8f4b3539700dca3cf7275f7f27521786316ec7
Juraj Trappl <juraj.trappl@proton.me>
2024-01-31 20:58:29 +0100

    functions code

--- projects/functions/src/main.rs
+++ projects/functions/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
     println!("Hello, world!");
+
+    another_function(5);
+
+    let y = {
+        let x = 1;
+        x + 1
+    };
+
+    println!("{y}");
+}
+
+fn another_function(x: i32) {
+    println!("The value of x: {x}");
 }
